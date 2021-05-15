6bf1daf127ba7ad7a1a6d2c6c8347e3115005052
PauloHenrique-UFPI <hunter404paulo@gmail.com>
2021-05-15 20:04:27 -0300

    att-6

---
+++ lista-8/cliente.py
@@ -0,0 +1,21 @@
+import socket
+
+ip = 'localhost'
+port = 8100
+
+addr = ((ip,port))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(addr)
+
+while True:
+    mensagem = input('Digite uma mensagem (caso digite ''sair'' a conexão e finalizada): ')
+    client_socket.send(mensagem.encode())
+
+    print('Mensagem enviada!!')
+    aux = client_socket.recv(1024).decode()
+    print(aux)
+
+    if(aux == 'Sair' or aux == 'sair'):
+        break
+
+client_socket.close()
